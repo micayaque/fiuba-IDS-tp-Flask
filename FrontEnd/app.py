80b8039d7a2ca13958e2ea321c275f1e06c7c2c8
micayaque <myaque@dc.uba.ar>
2025-06-02 16:50:22 -0300

    agrego app.py para mergear con la vista de materias

--- FrontEnd/app.py
+++ FrontEnd/app.py
@@ -1,10 +1,36 @@
-from flask import Flask, render_template, request, make_response, jsonify, url_for
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index ():
     return render_template('index.html')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run('localhost', port=8000, debug=True)

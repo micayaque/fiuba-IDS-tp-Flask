a049df034a2f5ab14f5002fdfb5123595206fc76
micayaque <myaque@dc.uba.ar>
2025-06-02 14:07:05 -0300

    cambio el nombre del archivo js

--- FrontEnd/static/js/script.js
+++ FrontEnd/static/js/script.js
@@ -1,27 +1,3 @@
-function mostrarPopup_contacto() {
-    document.getElementById("contacto_popup").style.display = "block";
-    document.getElementById("fondo_popupContacto").style.display = "block";
-};
-
-function cerrarPopup_contacto() {
-    document.getElementById("contacto_popup").style.display = "none";
-    document.getElementById("fondo_popupContacto").style.display = "none";
-};
-
-let correoSeleccionado = '';
-
-function seleccionar(correo) {
-  correoSeleccionado = correo;
-
-  // Actualiza la vista del boton seleccionado
-  document.querySelectorAll('#lista_correos button').forEach(btn => btn.classList.remove('seleccionado'));
-  const boton = Array.from(document.querySelectorAll('#lista_correos button')).find(b => b.textContent === correo);
-  if (boton) boton.classList.add('seleccionado');
-
-  console.log('Correo seleccionado:', correoSeleccionado);
-};
-
-
 // Formulario de Registro
 
 function togglePasswordVisibility() {

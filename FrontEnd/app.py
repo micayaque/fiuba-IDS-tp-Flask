a57784a7737f49744f72bf850f93247401c7adaa
micayaque <myaque@dc.uba.ar>
2025-06-02 15:07:42 -0300

    agrego bootstrap y soluciono problemas de estilos

--- FrontEnd/app.py
+++ FrontEnd/app.py
@@ -2,9 +2,11 @@ from flask import Flask, render_template, request, make_response, jsonify, url_f
 
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route("/grupos")
 def base ():
     return render_template('grupos_por_materia.html')
+
 
 if __name__ == '__main__':
     app.run('localhost', port=8000, debug=True)

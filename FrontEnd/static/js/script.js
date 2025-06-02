f33e3cc0e6616efb48141cb046ff01a2da0b3a76
micayaque <myaque@dc.uba.ar>
2025-06-02 03:12:10 -0300

    agrego estilos para el popup de contacto

--- FrontEnd/static/js/script.js
+++ FrontEnd/static/js/script.js
@@ -1,22 +1,10 @@
-function mostrarPopup_contacto() {
-    document.getElementById("contacto_popup").style.display = "block";
-    document.getElementById("fondo_popupContacto").style.display = "block";
+function mostrarPopupContacto() {
+    document.getElementById("contenedor_contacto_popup").style.display = "flex";
+    document.getElementById("fondo_popup_contacto").style.display = "block";
 };
 
-function cerrarPopup_contacto() {
-    document.getElementById("contacto_popup").style.display = "none";
-    document.getElementById("fondo_popupContacto").style.display = "none";
+function cerrarPopupContacto() {
+    document.getElementById("contenedor_contacto_popup").style.display = "none";
+    document.getElementById("fondo_popup_contacto").style.display = "none";
 };
 
-let correoSeleccionado = '';
-
-function seleccionar(correo) {
-  correoSeleccionado = correo;
-
-  // Actualiza la vista del boton seleccionado
-  document.querySelectorAll('#lista_correos button').forEach(btn => btn.classList.remove('seleccionado'));
-  const boton = Array.from(document.querySelectorAll('#lista_correos button')).find(b => b.textContent === correo);
-  if (boton) boton.classList.add('seleccionado');
-
-  console.log('Correo seleccionado:', correoSeleccionado);
-};

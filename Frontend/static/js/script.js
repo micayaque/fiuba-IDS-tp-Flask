a241d9eadc58a4b16ce44b145a4e2fb19de5e448
Camila <camilaetcpb@gmail.com>
2025-06-01 23:18:03 -0300

    se agrego los modals, grupos a los que pertenece el usuario, y se agrego la posibilidad de editar y eliminar imagenes de perfil y agregar materias al usuario

---
+++ Frontend/static/js/script.js
@@ -0,0 +1,61 @@
+//script para modals
+const modalCerrarSesion = document.getElementById("modal_cerrar_sesion");
+const botonMiPerfil = document.getElementById("botton_miperfil");
+const botonCerrarModal = document.getElementById("close_modal");
+
+botonMiPerfil.addEventListener("click", function() {
+    modalCerrarSesion.style.display = "flex";
+});
+
+botonCerrarModal.addEventListener("click", function() {
+    modalCerrarSesion.style.display = "none";
+    modalEditarPerfil.style.display = "none";
+    modalMateriasCursando.style.display = "none";
+    modalMateriasAprobadas.style.display = "none";
+    modalHorariosDisponibles.style.display = "none";
+});
+
+
+//modal editarfoto de perfil
+const modalEditarPerfil = document.getElementById("modal_editar_perfil");
+const botonEDITARMiPerfil = document.getElementById("boton_editar_foto_de_perfil");
+
+botonEDITARMiPerfil.addEventListener("click", function() {
+   modalEditarPerfil.style.display = "flex";
+});
+
+//modal materias cursando
+
+const modalMateriasCursando = document.getElementById("modal_materias_cursando");
+const botonMateriasCursando = document.getElementById("boton_editar_materias_cursando");
+
+botonMateriasCursando.addEventListener("click", function() {
+   modalMateriasCursando.style.display = "flex";
+});
+
+//modal materias aprobadas
+
+const modalMateriasAprobadas = document.getElementById("modal_materias_Aprobadas");
+const botonMateriasAprobadas = document.getElementById("boton_editar_materias_Aprobadas");
+
+botonMateriasAprobadas.addEventListener("click", function() {
+   modalMateriasAprobadas.style.display = "flex";
+});
+
+//modal horarios disponibles
+const modalHorariosDisponibles = document.getElementById("modal_horarios_disponibles");
+const botonHorariosDisponibles = document.getElementById("boton_editar_horarios_disponibles");
+
+botonHorariosDisponibles.addEventListener("click", function() {
+   modalHorariosDisponibles.style.display = "flex";
+});
+
+
+
+// Universal close for all modals
+document.querySelectorAll('.close_modal').forEach(btn => {
+    btn.addEventListener('click', function() {
+        // Cierra solo el modal al que pertenece el botón
+        this.closest('.desplegable').style.display = 'none';
+    });
+});

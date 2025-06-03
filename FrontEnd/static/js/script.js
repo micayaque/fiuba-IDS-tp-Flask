7b32e3cc5e771054e315ac11b000521067780ee3
micayaque <myaque@dc.uba.ar>
2025-06-03 16:18:37 -0300

    arreglo el filtro de materias

--- FrontEnd/static/js/script.js
+++ FrontEnd/static/js/script.js
@@ -15,6 +15,7 @@ function filtrarMaterias() {
   for (let i = 0; i < materias.length; i++) {
     // querySelector busca el elemento con clase 'materia-nombre' dentro del div actual (materias[i] es el div con clase 'materia-item' encontrado antes)
     const nombreElemento = materias[i].querySelector('.materia-nombre').textContent.toLowerCase();
+    
     materias[i].style.display = nombreElemento.startsWith(input) ? '' : 'none';
   }
 }

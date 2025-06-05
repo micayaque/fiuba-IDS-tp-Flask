20cd5949b1854dd1d77bb997a0e82eaf538a69f5
micayaque <myaque@dc.uba.ar>
2025-06-05 02:13:22 -0300

    agrego funciones js para cambiar el color del banner

--- Frontend/static/js/script.js
+++ Frontend/static/js/script.js
@@ -1,3 +1,4 @@
+// cambiar foto de perfil
 function cambiarFoto(nuevaFoto) {
     const fotoActual = document.querySelector('.foto_perfil_img');
     fotoActual.src = nuevaFoto;
@@ -6,3 +7,13 @@ function cambiarFoto(nuevaFoto) {
     const modalInstance = bootstrap.Modal.getInstance(modalElement);
     modalInstance.hide();
 }
+
+// cambiar el color del banner
+function abrirSelectorColor() {
+    document.getElementById('color_selector_banner').click();
+}
+
+function cambiarColorBanner(nuevoColor) {
+    document.getElementById('banner_perfil').style.backgroundColor = nuevoColor;
+}
+

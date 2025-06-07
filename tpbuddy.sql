0c45fbcfe4e6142388fe5a9e88904946ff1bb2af
micayaque <myaque@dc.uba.ar>
2025-06-07 18:24:18 -0300

    arreglo que el campo carrera puede ser null

--- tpbuddy.sql
+++ tpbuddy.sql
@@ -12,7 +12,7 @@ CREATE TABLE usuarios (
     contrasena VARCHAR(50) NOT NULL,
     nombre VARCHAR(50) NOT NULL,
     apellido VARCHAR(50) NOT NULL,
-    carrera VARCHAR(50) NOT NULL,
+    carrera VARCHAR(50),
     sobre_mi VARCHAR(1000),
     avatar_url VARCHAR(255),
     banner_color VARCHAR(7) DEFAULT '#A0B4B7'

1067116bc6d2f8cdc0d17b1bdd543b64a0aa5674
micayaque <myaque@dc.uba.ar>
2025-06-21 09:12:34 -0300

    cambio el modal de contacto para que sea parecido a los de registrarse/iniciar sesión

--- FrontEnd/static/js/script.js
+++ FrontEnd/static/js/script.js
@@ -1,15 +1,3 @@
-// Popup para el contacto en el menú 
-
-function mostrarPopupContacto() {
-    document.getElementById("contenedor_contacto_popup").style.display = "flex";
-    document.getElementById("fondo_popup_contacto").style.display = "block";
-}
-
-function cerrarPopupContacto() {
-    document.getElementById("contenedor_contacto_popup").style.display = "none";
-    document.getElementById("fondo_popup_contacto").style.display = "none";
-}
-
 // Filtrar las materias 
 
 function filtrarMaterias() {
